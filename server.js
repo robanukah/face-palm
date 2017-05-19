1a5d2763915a13270e43453d1d045ed892402d60
solairerove <hax2033@gmail.com>
2017-05-19 16:30:08 +0300

    Create connection with db

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+const MONGODB_URL = 'mongodb://localhost:27017'
 
-app.listen(3000, () => {
-  console.log('listening on 3000');
+var db
+
+MongoClient.connect(MONGODB_URL, (err, database) => {
+    // ... start da server
+    if (err) return console.log(err)
+    db = database
+
+    app.listen(3000, () => {
+        console.log('listening on 3000');
+    })
 })
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log(req.body)
+        res.redirect('/')
+    })
 })
