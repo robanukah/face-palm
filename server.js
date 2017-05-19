bcf1aee7196e9bc53f453ad2c1617658a52478f8
solairerove <hax2033@gmail.com>
2017-05-19 16:47:46 +0300

    Add ejs template support

--- server.js
+++ server.js
@@ -3,35 +3,37 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-const MONGODB_URL = 'mongodb://localhost:27017'
+const MONGODB_URL = 'mongodb://localhost:27017/quotes'
 
-var db
+let db
 
 MongoClient.connect(MONGODB_URL, (err, database) => {
-    // ... start da server
-    if (err) return console.log(err)
     db = database
 
     app.listen(3000, () => {
-        console.log('listening on 3000');
+        console.log('listening on 3000')
     })
 })
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(express.static('views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    db
+        .collection('quotes')
+        .find()
+        .toArray((err, result) => {
+            res.render('index.ejs', {quotes: result})
+        })
 })
 
 app.post('/quotes', (req, res) => {
-    db.collection('quotes').save(req.body, (err, result) => {
-        if (err) return console.log(err)
-
-        console.log(req.body)
-        res.redirect('/')
-    })
+    db
+        .collection('quotes')
+        .save(req.body, (err, result) => {
+            console.log(req.body)
+            res.redirect('/')
+        })
 })
