46cd0a2aa8f29351ec4e1492a612f34a0c1b5772
solairerove <hax2033@gmail.com>
2017-05-24 17:00:01 +0300

    Enable cors support

--- src/config/server.js
+++ src/config/server.js
@@ -1,6 +1,8 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export const expressConfig = (app) => {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
+    app.use(cors());
 };
