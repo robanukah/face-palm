9346720c3beaee97f64666d314f8fa83347b2c59
solairerove <hax2033@gmail.com>
2017-06-14 11:59:50 +0300

    #12 - DeprecationWarning: Mongoose: mpromise resolve: add bluebird promise support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import Promise from 'bluebird';
 
 import {postRouter} from './routes/post-router';
 
@@ -9,6 +10,7 @@ const app = express();
 const router = postRouter();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017');
+mongoose.Promise = Promise;
 
 mongoose
     .connection
@@ -32,10 +34,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-// register all routes
 app.use('/api', router);
 
-// start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('listening on ' + PORT);
